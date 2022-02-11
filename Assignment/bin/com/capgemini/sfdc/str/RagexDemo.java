52e998319b527a777f6683a56bd06e1fecc9a17c
abhishek8826 <99068746+abhishek8826@users.noreply.github.com>
2022-02-11 18:02:26 +0530

    Add files via upload

---
+++ Assignment/bin/com/capgemini/sfdc/str/RagexDemo.java
@@ -0,0 +1,40 @@
+package com.capgemini.sfdc.str;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class RagexDemo {
+	
+	public static void main(String[] args) {
+		String regex = "[A-Z]"; // 
+     	Pattern pattern = Pattern.compile(regex);
+//		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
+		for (int i = 33; i <= 126; i++) {
+			String testString = Character.toString((char) i);
+			Matcher matcher = pattern.matcher(testString);
+			System.out.println(i + " " + testString + " " + matcher.matches());
+		}
+
+
+	
+
+//		public static void main(String[] args) {
+//
+//			System.out.println("Start");
+//
+//			String regex = "abhi"; // what to search
+//			String input = "abhishek"; // where to search in?
+//
+//			Pattern p = Pattern.compile(regex);
+//
+//			Matcher m = p.matcher(input);
+//
+//			System.out.println(m.find()); // anywhere in the input - true
+//			System.out.println(m.lookingAt()); // in the beginning in the input - true
+//			System.out.println(m.matches()); // exact match - false
+//
+//			System.out.println("End");
+//		}
+
+	}
+}
+
---
+++ Assignment/bin/com/capgemini/sfdc/str/StringManipulationDemo.java
@@ -0,0 +1,35 @@
+package com.capgemini.sfdc.str;
+
+public class StringManipulationDemo {
+
+
+	//class Employee {
+//		int eid;
+	//}
+
+	
+
+		public static void main(String[] args) {
+
+			String str = "";
+			System.out.println(str);
+			System.out.println(str.hashCode());
+			str = "abc";
+			System.out.println(str);
+			System.out.println(str.hashCode());
+			str = "def";
+			System.out.println(str);
+			System.out.println(str.hashCode());
+
+//			Employee emp = new Employee();
+//			emp.eid = 101;
+//			System.out.println(emp.eid);
+//			System.out.println(emp.hashCode());
+//			emp.eid = 102;
+//			System.out.println(emp.eid);
+//			System.out.println(emp.hashCode());
+
+		}
+
+	}
+
