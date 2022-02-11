640072ec3fcb84b26c79e6e30db4f5a0e3f9490b
abhishek8826 <99068746+abhishek8826@users.noreply.github.com>
2022-02-11 17:57:20 +0530

    Add files via upload

---
+++ sfdc-java-demo4/src/com/capgemini/sfdc/Implementation.java
@@ -0,0 +1,53 @@
+package com.capgemini.sfdc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+class Candidate{
+	String name;
+	int id;
+	int age;
+	String gender;
+	String department;
+	int yearOfJoining;
+	double salary;
+	
+	Candidate(int id, String name, int age, String gender, String department, int yearOfJoining, double salary){
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.department = department;
+		this.yearOfJoining = yearOfJoining;
+		this.salary = salary;
+	}
+}
+	class Implementation {
+		public static void main(String[] args) {
+			
+		List <Candidate> list = new ArrayList<>();
+		
+		static Map<String, Long> Map = new Arraylist<>();
+		public getCount(List <Candidate> list) {
+			return Map<String, Long>;
+		}
+		
+		static Map<String, Double> = new Arraylist<>();  
+		public getAverageAge(List<Candidate> list) {
+		   return Map<String, Double>;
+		}
+		static Map<String, Long> =  new ArrayList<>();  
+		public countCandidatesDepartmentWise(List<Candidate> list) {
+		   return  Map<String, Long>;
+		}
+		static Optional<Candidate> = new Arraylist<>();
+		public getYoungestCandidateDetails(List<Candidate> list) {
+		   return Optional<Candidate> ;
+		}
+		   
+	    list.add(new Candidate(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0));
+	    list.add(new Candidate(144, "Scarlet Jhonson", 28, "Male", "Product Development", 2014, 32500.0));
+		
+		}
+	}
